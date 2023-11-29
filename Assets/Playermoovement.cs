b64c80fd26b0ce02fea7b0a556473d6dbff81018
Giacomo Deangelis <giacomo.deangelis@mail.polimi.it>
2023-11-29 17:40:31 +0100

    movimento personaggio + collisioni
    da aggiungere rilevamento del terreno e movimento un pò più fluido

--- Assets/Playermoovement.cs
+++ Assets/Playermoovement.cs
@@ -2,43 +2,56 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Playermoovement : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
-
-    public float velocitaMovimento = 5.0f; // Velocità di movimento del personaggio
-    public float sensibilitaRotazione = 2.0f; // Sensibilità della rotazione del personaggio
-    public Transform mainCamera; // Riferimento alla camera principale
+    public float moveSpeed = 5.0f;
+    public float rotationSpeed = 3.0f;
 
     private Rigidbody rb;
+    private Camera playerCamera;
 
     void Start()
     {
-        rb = GetComponent<Rigidbody>(); // Otteniamo il riferimento al Rigidbody del personaggio
-        if (mainCamera == null)
-        {
-            mainCamera = Camera.main.transform; // Se il riferimento alla camera non è stato assegnato, usiamo la camera principale
-        }
+        rb = GetComponent<Rigidbody>();
+        playerCamera = Camera.main;
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     void Update()
     {
-        // Movimento del personaggio in 3D con le frecce direzionali
-        float movimentoOrizzontale = Input.GetAxis("Horizontal");
-        float movimentoVerticale = Input.GetAxis("Vertical");
+        // Movimento del personaggio tramite frecce direzionali
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
 
-        Vector3 movimento = new Vector3(movimentoOrizzontale, 0.0f, movimentoVerticale);
-        rb.MovePosition(rb.position + transform.TransformDirection(movimento) * velocitaMovimento * Time.deltaTime);
+        Vector3 movement = new Vector3(horizontalInput, 0.0f, verticalInput) * moveSpeed * Time.deltaTime;
 
-        // Rotazione del personaggio con il mouse
-        float rotazioneMouseX = Input.GetAxis("Mouse X") * sensibilitaRotazione;
-        transform.Rotate(Vector3.up, rotazioneMouseX);
-
-        // Seguire il personaggio con la camera
-        if (mainCamera != null)
+        // Effettua il movimento solo se non c'è collisione in avanti
+        if (!IsCollidingForward())
         {
-            Vector3 nuovaPosizioneCamera = transform.position - transform.forward * 7.0f + Vector3.up * 5.0f;
-            mainCamera.position = Vector3.Lerp(mainCamera.position, nuovaPosizioneCamera, Time.deltaTime * 10.0f);
-            mainCamera.LookAt(transform.position + transform.forward * 30.0f);
+            rb.MovePosition(transform.position + transform.TransformDirection(movement));
         }
+
+        // Rotazione della visuale con il mouse
+        float mouseX = Input.GetAxis("Mouse X") * rotationSpeed;
+        transform.Rotate(Vector3.up * mouseX);
+
+        // Movimento della telecamera per seguire il personaggio
+        Vector3 cameraFollowPosition = transform.position - transform.forward * 5.0f + Vector3.up * 2.0f;
+        playerCamera.transform.position = Vector3.Lerp(playerCamera.transform.position, cameraFollowPosition, Time.deltaTime * 10.0f);
+        playerCamera.transform.LookAt(transform.position + transform.forward * 10.0f);
     }
-}
+
+    bool IsCollidingForward()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.forward, out hit, 1.0f))
+        {
+            // Controlla se la collisione è con un oggetto fisico
+            if (hit.collider.gameObject.GetComponent<Rigidbody>() != null)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+} 
