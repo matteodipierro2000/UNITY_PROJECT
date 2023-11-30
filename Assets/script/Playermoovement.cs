bf1260f17cce804700409c1b1f3259e5751525ff
Giacomo Deangelis <giacomo.deangelis@mail.polimi.it>
2023-11-30 18:57:05 +0100

    Camminata fluida
    Seeeeeee

--- Assets/script/Playermoovement.cs
+++ Assets/script/Playermoovement.cs
@@ -3,69 +3,52 @@ using System.Collections.Generic;
 using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
-    public float moveSpeed = 5.0f;
-    public float rotationSpeed = 3.0f;
-    public float distanceFromGround = 1.0f; // Distanza desiderata dal terreno
-    public LayerMask groundLayer; // LayerMask per il terreno
+    public float velocitaMovimento = 5.0f; // Velocità di movimento del personaggio
+    public float sensibilitaMouse = 2.0f; // Sensibilità della rotazione del personaggio
+    public float distanzaDaTerreno = 0.1f; // Distanza dal terreno
+    public LayerMask layerTerreno; // Layer del terreno
 
     private Rigidbody rb;
-    private Camera playerCamera;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        playerCamera = Camera.main;
-        Cursor.lockState = CursorLockMode.Locked;
-
-        // Assegna il Layer "Terrain" al LayerMask
-        groundLayer = LayerMask.GetMask("Terrain");
     }
 
     void Update()
     {
-        // Movimento del personaggio tramite frecce direzionali
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
+        // Movimento del personaggio con le frecce direzionali
+        float movimentoOrizzontale = Input.GetAxis("Horizontal");
+        float movimentoVerticale = Input.GetAxis("Vertical");
 
-        Vector3 movement = new Vector3(horizontalInput, 0.0f, verticalInput) * moveSpeed * Time.deltaTime;
+        Vector3 movimento = new Vector3(movimentoOrizzontale, 0.0f, movimentoVerticale);
+        rb.MovePosition(rb.position + transform.TransformDirection(movimento) * velocitaMovimento * Time.deltaTime);
 
-        // Effettua il movimento solo se non c'è collisione in avanti
-        if (!IsCollidingForward())
-        {
-            rb.MovePosition(transform.position + transform.TransformDirection(movement));
-        }
+        // Rotazione della visuale del personaggio con il mouse
+        float rotazioneMouseX = Input.GetAxis("Mouse X") * sensibilitaMouse;
+        transform.Rotate(Vector3.up, rotazioneMouseX);
 
-        // Rotazione della visuale con il mouse
-        float mouseX = Input.GetAxis("Mouse X") * rotationSpeed;
-        transform.Rotate(Vector3.up * mouseX);
-
-        // Movimento della telecamera per seguire il personaggio
-        Vector3 cameraFollowPosition = transform.position - transform.forward * 5.0f + Vector3.up * 2.0f;
-        playerCamera.transform.position = Vector3.Lerp(playerCamera.transform.position, cameraFollowPosition, Time.deltaTime * 10.0f);
-        playerCamera.transform.LookAt(transform.position + transform.forward * 10.0f);
-
-        // Adatta il personaggio al terreno
+        // Ottieni la normale del terreno sotto il personaggio
         RaycastHit hit;
-        if (Physics.Raycast(transform.position, -transform.up, out hit, distanceFromGround + 0.1f, groundLayer))
+        if (Physics.Raycast(transform.position, Vector3.down, out hit, 1.0f, layerTerreno))
         {
-            Vector3 targetPosition = hit.point + Vector3.up * distanceFromGround;
-            rb.MovePosition(targetPosition);
+            // Posizione desiderata (0.1f sopra il terreno)
+            Vector3 desiredPosition = hit.point + hit.normal * distanzaDaTerreno;
 
+            // Imposta la posizione del personaggio alla posizione desiderata
+            transform.position = desiredPosition;
+
+            // Rotazione del personaggio in base alla superficie del terreno
             Quaternion targetRotation = Quaternion.FromToRotation(transform.up, hit.normal) * transform.rotation;
-            rb.MoveRotation(Quaternion.Slerp(transform.rotation, targetRotation, 10.0f * Time.deltaTime));
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, 10 * Time.deltaTime);
         }
-    }
 
-    bool IsCollidingForward()
-    {
-        RaycastHit hit;
-        if (Physics.Raycast(transform.position, transform.forward, out hit, 0.5f))
+        // Seguire il personaggio con la camera
+        if (Camera.main != null)
         {
-            if (hit.collider.gameObject.layer != LayerMask.NameToLayer("Terrain"))
-            {
-                return true;
-            }
+            Vector3 nuovaPosizioneCamera = transform.position - transform.forward * 7.0f + Vector3.up * 5.0f;
+            Camera.main.transform.position = Vector3.Lerp(Camera.main.transform.position, nuovaPosizioneCamera, Time.deltaTime * 10.0f);
+            Camera.main.transform.LookAt(transform.position + transform.forward * 30.0f);
         }
-        return false;
     }
 }
